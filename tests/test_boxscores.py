21894e7525f556f1073ea1c5984684cbeaa330b9
matchavez <matchavez@gmail.com>
2026-06-26 09:50:29 +0000

    Add box-score gameid resolver; emit & commit boxscores.json from roster build

---
+++ tests/test_boxscores.py
@@ -0,0 +1,39 @@
+"""Tests for box-score gameid resolution (no network)."""
+from datetime import date
+
+from nzihl_rosters import boxscores
+
+
+SHELL = """
+<a href="/leagues/stats_1team.cfm?clientID=7131&teamID=674109&leagueID=35499&printPage=0"><b>Botany Swarm</b></a>
+<a href="/leagues/stats_1team.cfm?clientID=7131&teamID=674109&leagueID=35499&printPage=0"><b>BSW</b></a>
+NZIHL June 27th, 2026 4:45PM Avondale, Auckland
+<a href="/leagues/stats_1team.cfm?clientID=7131&teamID=674110&leagueID=35499&printPage=0"><b>Pure NZ Admirals</b></a>
+<a href="/leagues/stats_1team.cfm?clientID=7131&teamID=674110&leagueID=35499&printPage=0"><b>WAA</b></a>
+Game Number: 25
+"""
+
+SCHEDULE = """
+[FINAL](https://www.nzihl.com/leagues/hockey_boxscores.cfm?clientid=7131&leagueid=35499&gameid=2519935)
+[FINAL](https://www.nzihl.com/leagues/hockey_boxscores.cfm?clientid=7131&leagueid=35499&gameid=2519936)
+SAT 27 JUN tickets only, no boxscore link yet
+"""
+
+
+def test_parse_shell_orders_away_then_home_and_reads_date():
+    parsed = boxscores.parse_shell(SHELL)
+    assert parsed == {"away_id": 674109, "home_id": 674110, "date": date(2026, 6, 27)}
+
+
+def test_parse_shell_rejects_non_game_page():
+    assert boxscores.parse_shell("<html>no teams here, June stuff</html>") is None
+
+
+def test_last_final_gameid_picks_the_max():
+    assert boxscores.last_final_gameid(SCHEDULE) == 2519936
+
+
+def test_public_boxscore_url_shape():
+    url = boxscores.public_boxscore_url(2519937)
+    assert "hockey_boxscores.cfm" in url and "gameid=2519937" in url
+    assert "clientid=7131" in url and "leagueid=35499" in url
